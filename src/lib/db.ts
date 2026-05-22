f2c70fea39f3dcfb1fe81eeee00f835310eaf995
wilson3centaurus <tafadzwawilsonsedze@gmail.com>
2026-05-23 01:07:24 +0200

    more

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -3,4 +3,6 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
 const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE_KEY!
 
-export const supabase = createClient(supabaseUrl, supabaseServiceKey)
+export const supabase = createClient(supabaseUrl, supabaseServiceKey, {
+  db: { schema: 'aura' },
+})
